76c40b67326f84b00f4ca17df654df25541ea257
fpeyret <fpeyret@axome.com>
2019-09-28 15:40:58 +0200

    [+] Class for stats

---
+++ core/stat.js
@@ -0,0 +1,52 @@
+class Stat {
+    constructor(value,max,unit) {
+        this.value = value;
+        this.max = max;
+
+        if(typeof unit !== 'undefined') {
+            this.unit = unit;
+        } else {
+            this.unit = '%';
+        }
+
+        this.percentValue = null;
+
+        if(this.unit === '%')
+            this.setPercentValue();
+
+    }
+
+    get() {
+        return this.percentValue;
+    }
+
+    getText() {
+        if(this.unit === '%' && this.max > 0) {
+            return this.percentValue + ' ' + this.unit;
+        } else {
+            return this.value + ' ' + this.unit;
+        }
+    }
+
+    setValue(value) {
+        this.value = value;
+        if(this.max > 0) {
+            this.setPercentValue();
+        }
+    }
+
+    setMax(max) {
+        this.max = max;
+        if(this.unit === '%')
+            this.setPercentValue();
+    }
+
+    setPercentValue(value) {
+        this.percentValue = ceil((100 * this.value) / this.max);
+    }
+
+    getPercentValue() {
+        return this.percentValue;
+    }
+
+}
--- rules/atmosphere.js
+++ rules/atmosphere.js
@@ -1,46 +1,64 @@
 class Atmosphere {
     constructor() {
-        this.elements = {
-            "azote": {
+        this.elements = [
+            {
+                name: "azote",
                 qte: 78,
-                affichage: 'N2'
-            },
-            "oxygen": {
+                max: 100,
+                affichage: 'N2 :'
+            }, {
+                name: "oxygen",
                 qte: 20,
-                affichage: 'O2'
-            },
-            "argon": {
+                max: 100,
+                affichage: 'O2 :'
+            }, {
+                name: "argon",
                 qte: 1,
-                affichage: 'Ar'
-            },
-            "carbone": {
+                max: 100,
+                affichage: 'Ar :'
+            }, {
+                name: "carbone",
                 qte: 1,
-                affichage: 'CO2'
+                max: 100,
+                affichage: 'CO2 :'
             },
+        ];
+
+        for(let el of this.elements) {
+            el.stat = new Stat(el.qte,el.max);
         }
-        this.stats = {
-            "temperature": {
+
+        this.stats = [
+            {
+                name: "temperature",
                 qte: 15,
-                affichage: '°C'
-            },
-            "pression": {
+                max: null,
+                unit: 'C°',
+                affichage: ''
+            }, {
+                name: "pression",
                 qte: 1043,
-                max: 2500,
-                affichage: 'hPa'
+                max: null,
+                unit: 'hPa',
+                affichage: ''
             }
+        ]
+
+        for(let stat of this.stats) {
+            stat.stat = new Stat(stat.qte,stat.max,stat.unit);
         }
     }
 
     update() {
-        if(this.stats["pression"].qte >= this.stats["pression"].max) {
-            this.stats["pression"].qte = this.stats["pression"].max;
+        if(this.stats[1].qte >= this.stats[1].max) {
+            this.stats[1].qte = this.stats[1].max;
         } else {
             //this.stats["pression"].qte += 1;
         }
     }
 
     show(w) {
-        const atmoHeight = map(this.stats["pression"].qte,1000,1200,40,60);
+        const atmoHeight = map(this.stats[1].qte,1000,1200,40,60);
         ellipseMode(CENTER);
         fill(255,5);
         ellipse(0,0,w+atmoHeight*2,w+atmoHeight*2);
@@ -59,12 +77,24 @@ class Atmosphere {
         fill(255);
         noStroke();
         textSize(10);
-        text(this.elements["azote"].affichage+' : '+ceil(this.elements["azote"].qte)+'%', 15, height - 86);
+
+        let atomCoord = createVector(15, height - 24);
+        let offetY = 0;
+        for(let el of this.elements) {
+            text(el.affichage + ' ' + el.stat.getText(), atomCoord.x, atomCoord.y - (offetY * 12));
+            offetY++;
+        }
+        for(let stat of this.stats) {
+            text(stat.affichage + ' ' + stat.stat.getText(), atomCoord.x, atomCoord.y - (offetY * 12));
+            offetY++;
+        }
+
+        /*text(this.elements["azote"].affichage+' : '+ceil(this.elements["azote"].qte)+'%', 15, height - 86);
         text(this.elements["oxygen"].affichage+' : '+ceil(this.elements["oxygen"].qte)+'%', 15, height - 74);
         text(this.elements["argon"].affichage+' : '+ceil(this.elements["argon"].qte)+'%', 15, height - 62);
-        text(this.elements["carbone"].affichage+' : '+ceil(this.elements["carbone"].qte)+'%', 15, height - 50);
-        text(ceil(this.stats["pression"].qte)+''+this.stats["pression"].affichage, 15, height - 38);
-        text(ceil(this.stats["temperature"].qte)+''+this.stats["temperature"].affichage, 15, height - 26);
+        text(this.elements["carbone"].affichage+' : '+ceil(this.elements["carbone"].qte)+'%', 15, height - 50);*/
+        //text(ceil(this.stats["pression"].qte)+''+this.stats["pression"].affichage, 15, height - 38);
+        //text(ceil(this.stats["temperature"].qte)+''+this.stats["temperature"].affichage, 15, height - 26);
     }
 
 }
